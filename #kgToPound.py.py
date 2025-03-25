2fd6c6b2657057ef304349946190d620a1dca94d
KIM <dktm97@gmail.com>
2025-03-26 07:19:01 +0900

    16.연습문제 코딩하기기

---
+++ #kgToPound.py.py
@@ -0,0 +1,6 @@
+#kg값 입력,pound값 산출
+kg=float(input())
+pound=kg*2.20462
+
+#pound 값 print
+print(f"{pound} pound")
---
+++ CircleCal.py.py
@@ -0,0 +1,11 @@
+#반지름을 입력 받고,원의 넓이와 둘레를 계산하여 출력하는 프로그램
+
+r=float(input())
+
+pi=3.14
+
+area=pi*r**2
+cir=2*pi*r
+
+print(area)
+print(cir)
